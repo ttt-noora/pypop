1c9ec3adeed5f2f613e3691e4b267e17c3306a1a
ttt-noora <72697265+ttt-noora@users.noreply.github.com>
2024-05-16 16:30:15 +0800

    Update test_gs.py

--- pypop7/optimizers/rs/test_gs.py
+++ pypop7/optimizers/rs/test_gs.py
@@ -14,5 +14,5 @@ def test_optimize():
                'lr': 0.000001}
     gs = GS(problem, options)  # initialize the optimizer class
     results = gs.optimize()  # run the optimization process
-    # return the number of function evaluations and best-so-far fitness
-    print(f"GS: {results['n_function_evaluations']}, {results['best_so_far_y']}")
+    assert results['n_function_evaluations'] == 5000
+    assert results['best_so_far_y'] < 1.0
